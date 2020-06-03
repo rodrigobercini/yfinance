a4b640248bdf3d548b3ffbfc43b94be81a80c1af
Rodrigo Bercini Martins <rodrigobercinimartins@gmail.com>
2020-06-03 20:15:06 -0300

    https://www.mattbutton.com/2019/01/24/how-to-scrape-yahoo-finance-and-extract-fundamental-stock-market-data-using-python-lxml-and-pandas/

--- yfinance/base.py
+++ yfinance/base.py
@@ -26,6 +26,8 @@ import datetime as _datetime
 import requests as _requests
 import pandas as _pd
 import numpy as _np
+import lxml
+from lxml import html
 
 try:
     from urllib.parse import quote as urlencode
@@ -40,6 +42,85 @@ from . import utils
 
 from . import shared
 
+
+def get_page(url):
+    # Set up the request headers that we're going to use, to simulate
+    # a request by the Chrome browser. Simulating a request from a browser
+    # is generally good practice when building a scraper
+    headers = {
+        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3',
+        'Accept-Encoding': 'gzip, deflate, br',
+        'Accept-Language': 'en-US,en;q=0.9',
+        'Cache-Control': 'max-age=0',
+        'Pragma': 'no-cache',
+        'Referrer': 'https://google.com',
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/77.0.3865.120 Safari/537.36'
+    }
+
+    return _requests.get(url, headers=headers)
+
+def parse_rows(table_rows):
+    parsed_rows = []
+
+    for table_row in table_rows:
+        parsed_row = []
+        el = table_row.xpath("./div")
+
+        none_count = 0
+
+        for rs in el:
+            try:
+                (text,) = rs.xpath('.//span/text()[1]')
+                parsed_row.append(text)
+            except ValueError:
+                parsed_row.append(np.NaN)
+                none_count += 1
+
+        if (none_count < 4):
+            parsed_rows.append(parsed_row)
+            
+    return pd.DataFrame(parsed_rows)
+
+def clean_data(df):
+    df = df.set_index(0) # Set the index to the first column: 'Period Ending'.
+    df = df.transpose() # Transpose the DataFrame, so that our header contains the account names
+    
+    # Rename the "Breakdown" column to "Date"
+    cols = list(df.columns)
+    cols[0] = 'Date'
+    df = df.set_axis(cols, axis='columns', inplace=False)
+    
+    numeric_columns = list(df.columns)[1::] # Take all columns, except the first (which is the 'Date' column)
+
+    for column_index in range(1, len(df.columns)): # Take all columns, except the first (which is the 'Date' column)
+        df.iloc[:,column_index] = df.iloc[:,column_index].str.replace(',', '') # Remove the thousands separator
+        df.iloc[:,column_index] = df.iloc[:,column_index].astype(np.float64) # Convert the column to float64
+    df.index = df['Date']
+    df.drop('Date', axis=1, inplace=True)
+    df = df.transpose()
+        
+    return df
+
+def scrape_table(url):
+    # Fetch the page that we're going to parse
+    page = get_page(url);
+
+    # Parse the page with LXML, so that we can start doing some XPATH queries
+    # to extract the data that we want
+    tree = html.fromstring(page.content)
+
+    # Fetch all div elements which have class 'D(tbr)'
+    table_rows = tree.xpath("//div[contains(@class, 'D(tbr)')]")
+    
+    # Ensure that some table rows are found; if none are found, then it's possible
+    # that Yahoo Finance has changed their page layout, or have detected
+    # that you're scraping the page.
+    assert len(table_rows) > 0
+    
+    df = parse_rows(table_rows)
+    df = clean_data(df)
+        
+    return df
 
 class TickerBase():
     def __init__(self, ticker):
@@ -363,19 +444,9 @@ class TickerBase():
         data = utils.get_json(url+'/financials', proxy)
 
         # generic patterns
-        for key in (
-            (self._cashflow, 'cashflowStatement', 'cashflowStatements'),
-            (self._balancesheet, 'balanceSheet', 'balanceSheetStatements'),
-            (self._financials, 'incomeStatement', 'incomeStatementHistory')
-        ):
-
-            item = key[1] + 'History'
-            if isinstance(data.get(item), dict):
-                key[0]['yearly'] = cleanup(data[item][key[2]])
-
-            item = key[1]+'HistoryQuarterly'
-            if isinstance(data.get(item), dict):
-                key[0]['quarterly'] = cleanup(data[item][key[2]])
+        self._cashflow = scrape_table(f'https://finance.yahoo.com/quote/{ticker}/cash-flow?p={ticker}')
+        self._balancesheet = scrape_table(f'https://finance.yahoo.com/quote/{ticker}/balance-sheet?p={ticker}')
+        self._financials = scrape_table(f'https://finance.yahoo.com/quote/{ticker}/financials?p={ticker}')
 
         # earnings
         if isinstance(data.get('earnings'), dict):
@@ -523,3 +594,4 @@ class TickerBase():
 
         self._isin = data.split(search_str)[1].split('"')[0].split('|')[0]
         return self._isin
+    
